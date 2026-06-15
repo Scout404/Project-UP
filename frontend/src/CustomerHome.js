ec101e1e1a6ea440fda1ade0a84e7a36628145e5
user02300 <1060594@hr.nl>
2026-06-15 17:19:26 +0200

    search test en homepagina knoppen

--- frontend/src/CustomerHome.js
+++ frontend/src/CustomerHome.js
@@ -365,7 +365,7 @@ function CustomerHome({ user, onLogout, onLoginSuccess }) {
               <p className="eyebrow">New look</p>
               <h1>Discover new looks.</h1>
               <p className="hero-text">
-                Explore latest collection.
+                Explore our latest collection.
               </p>
             </div>
 
@@ -405,19 +405,27 @@ function CustomerHome({ user, onLogout, onLoginSuccess }) {
             </div>
           </section>
         )}
-        {/* About us PAGE */}
+        {/* support PAGE */}
         {page === "support" && (
           <section className="hero-panel" style={{ padding: "2rem" }}>
             <div className="hero-copy">
               <h1>Support</h1>
-              <h2>FAQ</h2> 
-              <h4>Where is my order?</h4>
-                <p>Orders take between 3-5 days to arrive, using the track and trace code you can follow your order.</p>
-              <h4>Item did not arrive</h4>
-                <p>contact our customer-service.</p>
+              <h2>FAQ</h2>
 
-              <h4>How many days do i have to return an item?</h4>
+              <details>
+                <summary>Where is my order?</summary>
+                <p>Orders take between 3-5 days to arrive, using the track and trace code you can follow your order.</p>
+              </details>
+
+              <details>
+                <summary>Item did not arrive</summary>
+                <p>Contact our customer-service.</p>
+              </details>
+
+              <details>
+                <summary>How many days do I have to return an item?</summary>
                 <p>You have 14 days to return an item.</p>
+              </details>
 
               <button className="primary-btn" onClick={() => setPage("home")}>
                 Back
@@ -425,6 +433,7 @@ function CustomerHome({ user, onLogout, onLoginSuccess }) {
             </div>
           </section>
         )}
+
 
         {/* PRODUCTS */}
         {showProducts && (
